a2af942189c33c0a8ce10d24b0dbd887c79bcddb
Jhon849 <jhon.moncayo@campusucc.edu.co>
2025-11-10 08:47:08 -0500

    Initial commit for backend deployment

---
+++ src/main/java/com/kicklite/app/KickliteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kicklite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickliteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickliteBackendApplication.class, args);
+	}
+
+}
